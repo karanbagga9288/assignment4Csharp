67f2793f9975a9c152c5a217150ac899350a25c1
karanbagga9288 <67565507+karanbagga9288@users.noreply.github.com>
2020-07-29 23:33:45 -0400

    Add files via upload

---
+++ Info.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebApplication4
+{
+    internal class Info : OpenApiInfo
+    {
+        public string Title { get; set; }
+        public string Version { get; set; }
+    }
+}
